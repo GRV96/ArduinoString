ccec98248ae6435c4debae617eea828f99c93903
guyllaume.rousseau.1@ens.etstml.ca <guyllaume.rousseau.1@ens.etsmtl.ca>
2018-05-16 13:31:56 -0400

    Début des tests, échec de l'opérateur =

--- include/string.h
+++ include/string.h
@@ -1,14 +1,20 @@
 #ifndef STRING_H
 #define STRING_H
 
-class string : public std::string
+#include <string>
+
+class String : public std::string
 {
     public:
         String();
+        String(const std::string& str);
+        String(unsigned int n, char c);
         virtual ~String();
-        char charAt(unsigned int indice) const;
-        unsigned int indexOf(char c) const;
-        unsigned int indexOf(char c, unsigned int indice) const;
+        String charAt(unsigned int indice) const;
+        int indexOf(char c, unsigned int indice=0) const;
+        int indexOf(const String& str, unsigned int indice=0) const;
+
+        String operator=(const char[] c);
 
     protected:
 
--- src/string.cpp
+++ src/string.cpp
@@ -1,16 +1,42 @@
-#include "string.h"
+#include "String.h"
 
-string::string()
+String::String()
 {
     //ctor
 }
 
-string::~string()
+String::String(const std::string& str): std::string(str)
+{
+    //
+}
+
+String::String(unsigned int n, char c): std::string(n, c)
+{
+    //ctor
+}
+
+String::~String()
 {
     //dtor
 }
 
-char string::charAt(unsigned int indice) const
+String String::charAt(unsigned int indice) const
 {
-    return at(indice);
+    char c = at(indice);
+    return String(1, c);
+}
+
+int String::indexOf(char c, unsigned int indice) const
+{
+    return find(c, indice);
+}
+
+int String::indexOf(const String& str, unsigned int indice) const
+{
+    return find(str, indice);
+}
+
+String String::operator =(const char[] c)
+{
+    return String(c);
 }
